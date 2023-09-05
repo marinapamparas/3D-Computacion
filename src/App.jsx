8cfdcc2a05d22b41261f7efe42312d825480ebb9
marielboher <mariellboheer@gmail.com>
2023-09-05 15:44:55 -0300

    compoenente services

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
-import './App.css'
-import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
-import NavBar from './components/Navbar/Navbar'
-
+import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import NavBar from "./components/Navbar/Navbar";
+import Servicios from "./components/Servicios/Servicios";
 
 function App() {
-  
   return (
     <div>
-
       <BrowserRouter>
-        
-        <NavBar/>
-       
+        <NavBar />
+        <Servicios />
         <Routes>
-
-          <Route path='*' element= {<Navigate to='/'/>} />
-
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-
       </BrowserRouter>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Servicios/Servicios.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import imgHerramientas from "../../../src/multimedia/herramientas.svg";
+import imgImpresora from "../../../src/multimedia/impresora.svg";
+import imgPc from "../../../src/multimedia/pc.svg";
+import imgCelular from "../../../src/multimedia/celular.svg";
+
+import "./servicios.css";
+
+const Servicios = () => {
+  return (
+    <div>
+      <div className="container-services">
+        <div className="title-services">
+          <h1>Servicios</h1>
+        </div>
+        <div className="container-info-services">
+          <div className="container-info">
+            <img src={imgHerramientas} />
+            <p>Reparación y mantenimiento de equipos de computación</p>
+          </div>
+          <div className="container-info">
+            <img src={imgImpresora} />
+            <p>Impresiones, scanner y fotocopias</p>
+          </div>
+          <div className="container-info">
+            <img src={imgPc} />
+            <p>Soporte técnico (harware)</p>
+          </div>
+          <div className="container-info">
+            <img src={imgCelular} />
+            <p>Reparación de celulares</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Servicios;
