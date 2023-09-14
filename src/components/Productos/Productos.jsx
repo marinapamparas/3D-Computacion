3867ce6b8510b2781bb704ce66a5080383201610
marielboher <mariellboheer@gmail.com>
2023-09-14 13:49:46 -0300

    servicios responsive!

--- src/components/Productos/Productos.jsx
+++ src/components/Productos/Productos.jsx
@@ -8,26 +8,31 @@ import imgMouse from "../../multimedia/mouse.png";
 
 const Productos = () => {
   return (
-    <div id='Productos'>
+    <div id="Productos">
       <div className="container-productos">
         <div className="info-productos">
-            <p><span>Productos </span> Conseguilos en nuestra tienda o por Marketplace. Consultanos; tenemos variedad de opciones.</p>
+          <h3 className="mobile-title">Productos</h3>
+          <p>
+            <span className="desktop-title">Productos </span> Conseguilos en
+            nuestra tienda o por Marketplace. Consultanos; tenemos variedad de
+            opciones.
+          </p>
         </div>
-          <div className="auris" data-label="Auriculares">
-            <img src={imgAuriculares} className="imagenHover"/>
-          </div>
-          <div className="cargador" data-label="Cargadores">
-          <img src={imgCargador} className="imagenHover"/>
-          </div>
-          <div className="fundas" data-label="Fundas y protectores para celular">
-            <img src={imgFundas} className="imagenHover"/>
-          </div>
-          <div className="cables" data-label="Cables USB y auxiliares">
-            <img src={imgCables} className="imagenHover"/>
-          </div>
-          <div className="mouse" data-label="Mouses y teclados">
-            <img src={imgMouse} className="imagenHover"/>
-          </div>
+        <div className="auris" data-label="Auriculares">
+          <img src={imgAuriculares} className="imagenHover" />
+        </div>
+        <div className="cargador" data-label="Cargadores">
+          <img src={imgCargador} className="imagenHover" />
+        </div>
+        <div className="fundas" data-label="Fundas y protectores para celular">
+          <img src={imgFundas} className="imagenHover" />
+        </div>
+        <div className="cables" data-label="Cables USB y auxiliares">
+          <img src={imgCables} className="imagenHover" />
+        </div>
+        <div className="mouse" data-label="Mouses y teclados">
+          <img src={imgMouse} className="imagenHover" />
+        </div>
       </div>
     </div>
   );
