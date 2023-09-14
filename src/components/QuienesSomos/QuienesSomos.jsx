d12a0b66795c110f510eaffb12be548ff0559863
Marina Pamparas <marpamparas@gmail.com>
2023-09-14 15:28:15 -0300

    modificaciones del html

--- src/components/QuienesSomos/QuienesSomos.jsx
+++ src/components/QuienesSomos/QuienesSomos.jsx
@@ -24,7 +24,7 @@ const QuienesSomos = () => {
                 <Col md={6} lg={true} className='containerp'>
                     <h2 className='fonth2'>¿Quiénes somos?</h2>
                     <p className='fontp'> Durante 20 años nos hemos dedicado apasionadamente al servicio de reparación y mantenimiento de computadoras en Las Heras, Mendoza. Nuestro éxito se basa en la cercanía que mantenemos con cada cliente. La confianza que depositan en nosotros es la base de nuestro compromiso diario para brindar soluciones tecnológicas que perduren en el tiempo. </p>
-                    <br />
+                    
                     <p className='fontp'>La calidad se refleja en cada detalle de nuestro trabajo y queremos asegurarnos que el servicio y los productos que ofrecemos a nuestros clientes sean excepcionales. </p>
                 </Col>
   
